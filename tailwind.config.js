bbf34456a04a25d417b4bb827d4708b8463a0f25
Facundo Juliá <facundodirie@gmail.com>
2023-05-06 17:24:30 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./public/*.{html,js}"],
   theme: {
     extend: {
       colors:{
-
         // primary colors
         'green-primary': '#B9D74D',
         'green-primary-dark': '#8BB741',
